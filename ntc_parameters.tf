2f878e2569a7bfaefb652f6f65a0725fa4fcf5f4
stefano-franco <stefano.franco@nuvibit.com>
2023-04-12 17:19:17 +0200

    update parameters

--- ntc_parameters.tf
+++ ntc_parameters.tf
@@ -1,8 +1,12 @@
 locals {
   ntc_parameters = module.ntc_parameters_reader.parameter_map
+  # parameters that are managed by log-archive account
   ntc_parameters_log_archive = {
-    # this node contains module parameters which are provisioned in core_log_archive account
-    "log_archive" : { "input1" : "value1", "input2" : ["value2"], "input3" : 3 },
+    "log_archive_module" : { 
+      "input1" : "value1", 
+      "input2" : ["value2"], 
+      "input3" : 3 
+    },
   }
 }
 
@@ -22,6 +26,6 @@ module "ntc_parameters_reader" {
 #   source = "github.com/nuvibit/terraform-aws-ntc-parameters//modules/writer?ref=beta"
 
 #   bucket_name     = "ntc-parameters-c2"
-#   parameter_node  = "connectivity"
-#   node_parameters = local.ntc_parameters_connectivity
+#   parameter_node  = "log-archive"
+#   node_parameters = local.ntc_parameters_log_archive
 # }
