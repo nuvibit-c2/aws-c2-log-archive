993bf1803545afe84eacd36820558cbd8129de9f
stefano-franco <stefano.franco@nuvibit.com>
2023-04-16 21:44:07 +0200

    new parameters bucket

--- ntc_parameters.tf
+++ ntc_parameters.tf
@@ -21,7 +21,7 @@ locals {
 module "ntc_parameters_reader" {
   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-parameters//modules/reader?ref=beta"
 
-  bucket_name = "ntc-parameters-c2"
+  bucket_name = "aws-c2-ntc-parameters"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -30,7 +30,7 @@ module "ntc_parameters_reader" {
 module "ntc_parameters_writer" {
   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-parameters//modules/writer?ref=beta"
 
-  bucket_name     = "ntc-parameters-c2"
+  bucket_name     = "aws-c2-ntc-parameters"
   parameter_node  = "log-archive"
   node_parameters = local.ntc_parameters_log_archive
 }
