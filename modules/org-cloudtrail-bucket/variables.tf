0000dc5586973a9eeb3b0cfb9a4e49654660d470
Jonas Saegesser <jonas.saegesser@nuvibit.com>
2023-04-23 19:27:19 +0200

    feat(cloudtrail-bucket): add conditions to org_cloudtrail_region and org_cloudtrail_bucket_kms_alias

--- modules/org-cloudtrail-bucket/variables.tf
+++ modules/org-cloudtrail-bucket/variables.tf
@@ -4,7 +4,7 @@ variable "cloudtrail_admin_account_id" {
 
   validation {
     condition     = can(regex("^[0-9]{12}$", var.org_mgmt_account_id))
-    error_message = "\"org_mgmt_account_id\" needs to be a valid Account ID: 12 digit number"
+    error_message = "\"org_mgmt_account_id\" needs to be a valid Account ID: 12 digit number."
   }
 }
 
@@ -14,7 +14,7 @@ variable "org_cloudtrail_name" {
   default = "org_cloudtrail"
 
   validation {
-    condition     = can(regex("^[a-zA-Z0-9]([0-9A-Za-z]|[\._-](?![\._-])){1,126}[a-zA-Z0-9]$", var.org_mgmt_account_id))
+    condition     = can(regex("^[a-zA-Z0-9]([0-9A-Za-z]|\\._-](?![\\._-])){1,126}[a-zA-Z0-9]$", var.org_mgmt_account_id))
     error_message = <<EOT
     \"org_cloudtrail_name\" needs to be a valid cloutrail name:
     - Contain only ASCII letters (a-z, A-Z), numbers (0-9), periods (.), underscores (_), or dashes (-)
@@ -29,13 +29,19 @@ variable "org_cloudtrail_region" {
   type = string
   description = "Region where the Organization CloudTrail will be deployed"
 
-  //TODO Condition
+  validation {
+    condition = can(regex("(us(-gov)?|ap|ca|cn|eu|sa)-(central|(north|south)?(east|west)?)-\d"), var.org_cloudtrail_region)
+    error_message = "\"org_cloudtrail_region\" needs to be a valid region code (eu-central-1, us-east-1, etc)."
+  }
 }
 
 variable "org_cloudtrail_bucket_kms_alias" {
   type = string
   description = "Alias for ClouTrail bucket KMS key alias"
 
-  //TODO Condition
+  validation  {
+    condition = can (regex("^[a-zA-Z0-9\\/_-]{1,250}$"))
+    error_message = "\"org_cloudtrail_bucket_kms_alias\" must only contain ASCII letters (a-z, A-Z), numbers (0-9), slashes (/), underscores (_), or dashes (-)"
+  }
 }
 
