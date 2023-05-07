0676cf351a2b62f905324066208d8ccdc4e61049
stefano-franco <stefano.franco@nuvibit.com>
2023-05-07 21:17:54 +0200

    init pipeline repo

---
+++ ntc_parameters.tf
@@ -0,0 +1,42 @@
+locals {
+  ntc_parameters_bucket_name = "aws-c2-ntc-parameters"
+  ntc_parameters_writer_node = "log-archive"
+
+  # map of parameters merged from all parameter nodes
+  ntc_parameters = module.ntc_parameters_reader.all_parameters
+
+  # parameters that are managed by core log archive account
+  ntc_parameters_to_write = {
+    bucket_arns : {
+      cloudtrail : ""
+      config : ""
+      guardduty : ""
+      security_hub : ""
+      flow_logs : ""
+      dns_logs : ""
+    }
+  }
+
+  # by default existing node parameters will be merged with new parameters to avoid deleting parameters
+  replace_parameters = true
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ NTC PARAMETERS - READER
+# ---------------------------------------------------------------------------------------------------------------------
+module "ntc_parameters_reader" {
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-parameters//modules/reader?ref=beta"
+
+  bucket_name = local.ntc_parameters_bucket_name
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ NTC PARAMETERS - WRITER
+# ---------------------------------------------------------------------------------------------------------------------
+module "ntc_parameters_writer" {
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-parameters//modules/writer?ref=beta"
+
+  bucket_name     = local.ntc_parameters_bucket_name
+  parameter_node  = local.ntc_parameters_writer_node
+  node_parameters = local.ntc_parameters_to_write
+}
