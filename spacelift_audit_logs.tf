f24e8ad936f0e4a206918e7c4cf414f2f57d18c3
stefano-franco <stefano.franco@nuvibit.com>
2024-11-26 16:30:02 +0100

    update prefix name

--- spacelift_audit_logs.tf
+++ spacelift_audit_logs.tf
@@ -4,8 +4,9 @@
 module "spacelift_audit_log_forwarder_euc1" {
   source = "./modules/spacelift-audit-log-forwarder"
 
-  forwarding_endpoint = "lambda_function_url"
-  audit_trail_secret  = "1k2l3hjrkjh12lkjasd"
+  forwarder_name_prefix = "spacelift-audit-log-forwarder-euc1"
+  forwarding_endpoint   = "lambda_function_url"
+  audit_trail_secret    = "1k2l3hjrkjh12lkjasd"
 
   providers = {
     aws = aws.euc1
@@ -23,8 +24,9 @@ output "spacelift_audit_log_forwarder_euc1" {
 module "spacelift_audit_log_forwarder_euc2" {
   source = "./modules/spacelift-audit-log-forwarder"
 
-  forwarding_endpoint = "lambda_function_url"
-  audit_trail_secret  = "1k2l3hjrkjh12lkjasd"
+  forwarder_name_prefix = "spacelift-audit-log-forwarder-euc2"
+  forwarding_endpoint   = "lambda_function_url"
+  audit_trail_secret    = "1k2l3hjrkjh12lkjasd"
 
   providers = {
     aws = aws.euc2
