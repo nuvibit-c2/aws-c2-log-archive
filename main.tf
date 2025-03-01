f3aceac99caa7ff2cada0054d1c00b089eb8ee28
stefano-franco <stefano.franco@nuvibit.com>
2025-03-01 14:56:55 +0100

    test provider

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = "~> 5.33"
+      version               = "~> 5.33, = 5.88"
       configuration_aliases = []
     }
   }
